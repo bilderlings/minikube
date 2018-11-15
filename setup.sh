dc99e79d4cfa5fb3cd1017cd5d2d4d20166989a1
Vladislavs Korehovs <v.korehovs@bilderlings.eu>
2018-11-15 09:10:57 +0200

    fixing misc things

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 
 if [ -x "$(command -v minikube)" ]; then
     echo "Minikube already exists, performing re-install"
-    if [ "`which minikube`" -ne "/usr/local/bin/minikube" ]; then
+    if [[ "`which minikube`" != "/usr/local/bin/minikube" ]]; then
         echo "Already installed minikube yourself in non-standard location? please remove it"
         exit -1
     fi
@@ -43,8 +43,8 @@ else
     sudo apt-get update
     sudo apt-get install docker-ce=18.06.0~ce~3-0~ubuntu
 fi
-
 echo "Starting minikube installation"
+
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo cp kubectl /usr/local/bin/ && rm kubectl
 
@@ -67,7 +67,17 @@ for i in {1..150}; do # timeout for 5 minutes
   fi
   sleep 2
 done
+
+# enable ingress
+sudo minikube addons enable ingress
+
+# install helm
+https://github.com/helm/helm/releases/tag/v2.11.0
+curl https://github.com/helm/helm/releases/tag/v2.11.0 --output helm.tar.gz && tar -xzvf helm.tar.gz
+
 echo "#!/usr/bin/env bash" >$HOME/minikube-ctx.sh
 echo "export KUBECONFIG=$HOME/.kube/config.minikube" >>$HOME/minikube-ctx.sh
 chmod +x $HOME/minikube-ctx.sh
-echo "minikube setup was successfull, please run ~/minikube-ctx.sh before you will use kubectl, it will set your context accordingly"
+set +x
+>&2 echo "minikube setup was successfull, please run ~/minikube-ctx.sh before you will use kubectl, it will set your context accordingly"
+set -x
