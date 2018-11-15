c8341a96ed6861da9c217cf54bea91764da67e57
Vladislavs Korehovs <v.korehovs@bilderlings.eu>
2018-11-15 16:18:10 +0200

    removing typo

--- cert-manager-setup.sh
+++ cert-manager-setup.sh
@@ -10,8 +10,6 @@ kubectl delete customresourcedefinitions.apiextensions.k8s.io certificates.certm
 kubectl delete customresourcedefinitions.apiextensions.k8s.io clusterissuers.certmanager.k8s.io || true
 kubectl delete customresourcedefinitions.apiextensions.k8s.io issuers.certmanager.k8s.io || true
 
-tiller-deploy
-
 helm install --wait \
     --name cert-manager \
     --namespace kube-system \
