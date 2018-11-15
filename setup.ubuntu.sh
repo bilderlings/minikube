da371c3badaf350265f16177df9417890f9d6293
Vladislavs Korehovs <v.korehovs@bilderlings.eu>
2018-11-15 18:56:12 +0200

    fixing version of minikube and kubectl

--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -75,9 +75,14 @@ if [ -x "$(command -v kubectl)" ]; then
     sudo rm /usr/local/bin/kubectl
 fi
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo cp kubectl /usr/local/bin/ && rm kubectl
-
+curl -Lo minikube https://github.com/kubernetes/minikube/releases/download/v0.30.0/minikube-linux-amd64 && \
+ chmod +x minikube && \
+ sudo cp minikube /usr/local/bin/ && \
+ rm minikube
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.12.2/bin/linux/amd64/kubectl && \
+ chmod +x kubectl && \
+ sudo cp kubectl /usr/local/bin/ && \
+ rm kubectl
 
 # install hidden deps of kubectl
 sudo apt-get install socat
