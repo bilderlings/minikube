4cf7ed1b387984024ebbac65277dbf14a4afb9a1
Vladislavs Korehovs <v.korehovs@bilderlings.eu>
2018-11-15 16:10:43 +0200

    adding wait

--- cert-manager-setup.sh
+++ cert-manager-setup.sh
@@ -9,6 +9,9 @@ helm del --purge cert-manager || true
 kubectl delete customresourcedefinitions.apiextensions.k8s.io certificates.certmanager.k8s.io || true
 kubectl delete customresourcedefinitions.apiextensions.k8s.io clusterissuers.certmanager.k8s.io || true
 kubectl delete customresourcedefinitions.apiextensions.k8s.io issuers.certmanager.k8s.io || true
+
+tiller-deploy
+
 helm install --wait \
     --name cert-manager \
     --namespace kube-system \
--- helm-setup.sh
+++ helm-setup.sh
@@ -41,6 +41,8 @@ curl https://storage.googleapis.com/kubernetes-helm/$HELM_RELEASE.tar.gz --outpu
 
 helm init --upgrade
 
+kubectl wait --timeout=60s --for condition=ready  pod -n kube-system -l name=tiller
+
 set +x
 >&2 echo "Successfully setted-up helm, please execute ~/minikube-ctx.sh and then helm --help to start using it."
 set -x
