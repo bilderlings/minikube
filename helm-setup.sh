73e7e1e9a8a7705c11ff49d6ef35f174a01eb51f
Vladislavs Korehovs <v.korehovs@bilderlings.eu>
2018-11-16 11:22:53 +0200

    fixing helm

--- helm-setup.sh
+++ helm-setup.sh
@@ -9,10 +9,21 @@ if [ -x "$(command -v helm)" ]; then
         echo "Already installed helm yourself? please remove it"
         echo "Used apt? please run 'sudo apt remove helm'"
         echo "Simply installed it not in /usr/local/bin? please run 'sudo rm `which helm`'"
-        echo "Close & Open your current termina after this is done"
+        echo "Close & Open your current terminal after this is done"
         exit -1
     fi
+
+    read -p "Going to wipe out your existing helm installation and settings (y/n)? " answer
+    case ${answer:0:1} in
+        y|Y )
+            echo Yes
+        ;;
+        * )
+            exit -1
+        ;;
+    esac
     sudo rm /usr/local/bin/helm
+    sudo rm -fr /home/vkorehovs/.helm || true
 fi
 if [ -x "$(command -v tiller)" ]; then
     echo "Tiller already exists, performing re-install"
@@ -24,6 +35,15 @@ if [ -x "$(command -v tiller)" ]; then
         echo "Close & Open your current termina after this is done"
         exit -1
     fi
+    read -p "Going to wipe out your existing tiller installation and settings (y/n)? " answer
+    case ${answer:0:1} in
+        y|Y )
+            echo Yes
+        ;;
+        * )
+            exit -1
+        ;;
+    esac
     sudo rm /usr/local/bin/tiller
 fi
 
@@ -41,7 +61,6 @@ curl https://storage.googleapis.com/kubernetes-helm/$HELM_RELEASE.tar.gz --outpu
 
 helm init --upgrade
 kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
-#kubectl wait --timeout=60s --for condition=ready  pod -n kube-system -l name=tiller
 
 set +x
 >&2 echo "Successfully setted-up helm, please execute ~/minikube-ctx.sh and then helm --help to start using it."
